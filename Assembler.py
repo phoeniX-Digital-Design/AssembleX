e900a6a80d83ec9c8179e4db2b202577647645e0
Arvin Delavari <arvin7807@gmail.com>
2023-09-22 19:44:12 +0330

    Update Assembler script

--- Assembler.py
+++ Assembler.py
@@ -3,13 +3,13 @@ INSTRUCTIONS = {
     "add": {"opcode": "0110011", "funct3": "000", "funct7": "0000000"},
     "sub": {"opcode": "0110011", "funct3": "000", "funct7": "0100000"},
     "and": {"opcode": "0110011", "funct3": "111", "funct7": "0000000"},
-    "or":  {"opcode": "0110011", "funct3": "110", "funct7": "0000000"},
+    "or" : {"opcode": "0110011", "funct3": "110", "funct7": "0000000"},
     "xor": {"opcode": "0110011", "funct3": "100", "funct7": "0000000"},
     "sll": {"opcode": "0110011", "funct3": "001", "funct7": "0000000"},
 
     # I-Type Instructions
     "addi": {"opcode": "0010011", "funct3": "000"},
-    "ori" :  {"opcode": "0010011", "funct3": "110"},
+    "ori" : {"opcode": "0010011", "funct3": "110"},
     "andi": {"opcode": "0010011", "funct3": "111"},
 
     # U-Type Instructions
@@ -31,28 +31,72 @@ INSTRUCTIONS = {
     "ecall": {"opcode": "1110011", "funct3": "000", "funct7": "0000000"},
 }
 
-def assemble(instruction, registers):
-    opcode = INSTRUCTIONS[instruction]["opcode"]
-    funct3 = INSTRUCTIONS[instruction].get("funct3", "")
-    funct7 = INSTRUCTIONS[instruction].get("funct7", "")
+REGISTERS = {
+    "zero": 0,
+    "ra": 1,
+    "sp": 2,
+    "gp": 3,
+    "tp": 4,
+    "t0": 5,
+    "t1": 6,
+    "t2": 7,
+    "s0": 8,
+    "s1": 9,
+    "a0": 10,
+    "a1": 11,
+    "a2": 12,
+    "a3": 13,
+    "a4": 14,
+    "a5": 15,
+    "a6": 16,
+    "a7": 17,
+    "s2": 18,
+    "s3": 19,
+    "s4": 20,
+    "s5": 21,
+    "s6": 22,
+    "s7": 23,
+    "s8": 24,
+    "s9": 25,
+    "s10": 26,
+    "s11": 27,
+    "t3": 28,
+    "t4": 29,
+    "t5": 30,
+    "t6": 31,
+}
 
-    if instruction in ["lui", "auipc"]:
-        rd, imm = registers
-        return f"{opcode}{imm:020b}{rd:05b}"
-    elif instruction in ["jal"]:
-        rd, imm = registers
-        return f"{opcode}{imm:020b}{rd:05b}"
-    elif instruction in ["jalr"]:
-        rd, rs1, imm = registers
-        return f"{opcode}{imm:012b}{rs1:05b}{rd:05b}"
+def determine_format_type(instruction):
+    if instruction in ["add", "sub", "and", "or", "xor", "sll"]:
+        return "R"
+    elif instruction in ["addi", "ori", "andi"]:
+        return "I"
     elif instruction in ["sw", "sb"]:
-        rs2, imm, rs1 = registers
-        return f"{opcode}{imm[0:7]}{rs1:05b}{rs2:05b}{imm[7:12]}{funct3}"
+        return "S"
+    elif instruction in ["beq", "bne"]:
+        return "B"
+    elif instruction in ["lui", "auipc"]:
+        return "U"
+    elif instruction == "jal":
+        return "J"
+    elif instruction == "ecall":
+        return "System"
     else:
-        rd, rs1, imm = registers
-        return f"{opcode}{imm:012b}{rs1:05b}{rd:05b}{funct7}{funct3}"
+        raise ValueError("Invalid instruction")
 
-# Example usage
-instruction = "addi"
-registers = (1, 2, 10)
-print(assemble(instruction, registers))  # Output: 00100110001000010000000000001010
+def assemble_r_type(instruction, rd, rs1, rs2):
+    opcode = INSTRUCTIONS[instruction]["opcode"]
+    funct3 = INSTRUCTIONS[instruction]["funct3"]
+    funct7 = INSTRUCTIONS[instruction]["funct7"]
+    return f"{funct7}{rs2}{rs1}{funct3}{rd}{opcode}"
+
+def assemble_i_type(instruction, rd, rs1, imm):
+    opcode = INSTRUCTIONS[instruction]["opcode"]
+    funct3 = INSTRUCTIONS[instruction]["funct3"]
+    imm_bin = format(imm, '012b')
+    return f"{imm_bin}{rs1}{funct3}{rd}{opcode}"
+
+def assemble_s_type(instruction, rs1, rs2,):
+    opcode = INSTRUCTIONS[instruction]["opcode"]
+    funct3 = INSTRUCTIONS[instruction]["funct3"]
+    funct7 = INSTRUCTIONS[instruction]["funct7"]

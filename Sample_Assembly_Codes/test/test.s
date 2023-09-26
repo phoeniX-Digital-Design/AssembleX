ed76aac6d36424fdb463dd70e6e31492379d6863
ArvinDelavari <arvin7807@gmail.com>
2023-09-26 17:26:26 +0330

    🐛 Branch issue fixed (J-types issue under test)

--- Sample_Assembly_Codes/test/test.s
+++ Sample_Assembly_Codes/test/test.s
@@ -1,12 +1,7 @@
-.globl dot
-.text
-
-dot:
-    addi sp, sp, -24
-    sw s0, 0(sp)        #sum
-    sw s1, 4(sp)        #loaded val v0
-    sw s2, 8(sp)        #loaded val v1
-    sw s3, 12(sp)       #product
-    sw s4, 16(sp)       #stride v0
-    sw s5, 20(sp)       
-    addi t5, x0, 1		
+addi s0 x0 10 
+loop:
+	addi s0 s0 -1 
+	beq s1 x0 out 
+	beq x0 x0 loop
+out:
+	beq x0 x0 out

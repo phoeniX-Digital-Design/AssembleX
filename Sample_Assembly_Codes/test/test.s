60755fe259e04a2593bd8929723f0e13e5ba8716
ArvinDelavari <arvin7807@gmail.com>
2023-09-26 16:41:50 +0330

    Run test for AssembleX

--- Sample_Assembly_Codes/test/test.s
+++ Sample_Assembly_Codes/test/test.s
@@ -1,13 +1,12 @@
-.globl matmul
+.globl dot
 .text
-matmul:
-    addi sp, sp, -36
-    sw s0, 0(sp)
-    sw s1, 4(sp)
-    sw s2, 8(sp)
-    sw s3, 12(sp)
-    sw s4, 16(sp)
-    sw s5, 20(sp)
-    sw s6, 24(sp)
-    sw s7, 28(sp) 
-    sw ra, 32(sp)
+
+dot:
+    addi sp, sp, -24
+    sw s0, 0(sp)     
+    sw s1, 4(sp)        
+    sw s2, 8(sp)        
+    sw s3, 12(sp)       
+    sw s4, 16(sp)       
+    sw s5, 20(sp)       
+    addi t5, x0, 1		

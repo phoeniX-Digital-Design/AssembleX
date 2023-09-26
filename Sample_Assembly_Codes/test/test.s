1ac760881df28ad115cd02701ac044e941f81106
ArvinDelavari <arvin7807@gmail.com>
2023-09-26 17:02:12 +0330

    Running tests (issue with J/B type instructions)

--- Sample_Assembly_Codes/test/test.s
+++ Sample_Assembly_Codes/test/test.s
@@ -3,10 +3,10 @@
 
 dot:
     addi sp, sp, -24
-    sw s0, 0(sp)     
-    sw s1, 4(sp)        
-    sw s2, 8(sp)        
-    sw s3, 12(sp)       
-    sw s4, 16(sp)       
+    sw s0, 0(sp)        #sum
+    sw s1, 4(sp)        #loaded val v0
+    sw s2, 8(sp)        #loaded val v1
+    sw s3, 12(sp)       #product
+    sw s4, 16(sp)       #stride v0
     sw s5, 20(sp)       
     addi t5, x0, 1		

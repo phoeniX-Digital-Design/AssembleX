14e12ac7c21e9e1bd714c8c7c7ccd131296b56ee
ArvinDelavari <arvin7807@gmail.com>
2024-07-31 19:01:34 +0330

    Update address_mapping.py

--- address_mapping.py
+++ address_mapping.py
@@ -1,3 +1,13 @@
+#  AssembleX V3.0
+#  RISC-V Assembly Software Assistant for the phoeniX project (https://github.com/phoeniX-Digital-Design/phoeniX)
+
+#  Description: Address mapping and translation functions
+#  Copyright 2024 Iran University of Science and Technology. <phoenix.digital.electronics@gmail.com>
+
+#  Permission to use, copy, modify, and/or distribute this software for any
+#  purpose with or without fee is hereby granted, provided that the above
+#  copyright notice and this permission notice appear in all copies.
+
 def define_reset_address(first_line):
     start_address = first_line.split()
     try:
@@ -16,31 +26,30 @@ def define_reset_address(first_line):
         print('WARNING: .RESET_ADDRESS not defined. -> .RESET_ADDRESS overridden to 0x00000000\n')
         return 0
     
-def label_mapping(baseaddr, line, instrcnt, exp_instrcnt, labelcnt, label_list, label_addr_list):
+def label_mapping(base_address, line, instrcnt, expected_instructions_count, label_counter, label_list, label_address_list):
     words = line.split()
     # Check if blank line or comment or start_address
     try:
         if words[0][0] == '#':
             # Ignore comment and move on
             return 0
-        elif words[0] == '.RESET_ADDRESS' or words[0] == '.start_address':
+        elif words[0] == '.RESET_ADDRESS':
             # Ignore start_address
             return 0
     except:
         # Ignore blank line and move on
         return 0
 
-    # Valid label?
-    if len(words) == 1 and line[-1] == ':':  # Decode labels like 'mylabel:'
+    if len(words) == 1 and line[-1] == ':':
         label = line.split(':')[0]
-        labelcnt[0] = labelcnt[0] + 1
+        label_counter[0] = label_counter[0] + 1
         label_list.append(label)
-        label_addr_list.append(baseaddr + int(exp_instrcnt[0]) * 4)
+        label_address_list.append(base_address + int(expected_instructions_count[0]) * 4)
     elif len(words) > 1 and words[0][-1] == ':' and words[1][0] == '#':  # Decodes labels with comments
         label = line.split(':')[0]
-        labelcnt[0] = labelcnt[0] + 1
+        label_counter[0] = label_counter[0] + 1
         label_list.append(label)
-        label_addr_list.append(baseaddr + int(exp_instrcnt[0]) * 4)
+        label_address_list.append(base_address + int(expected_instructions_count[0]) * 4)
     else:
         # It is an instruction
         instrcnt[0] = instrcnt[0] + 1
@@ -48,10 +57,10 @@ def label_mapping(baseaddr, line, instrcnt, exp_instrcnt, labelcnt, label_list, 
             offset = 2  # Because LI = expands to two instructions
         else:
             offset = 1
-        exp_instrcnt[0] = exp_instrcnt[0] + offset
+        expected_instructions_count[0] = expected_instructions_count[0] + offset
 
-def address_mapping(labelcnt, label_list, label_addr_list):
+def address_mapping(label_counter, label_list, label_address_list):
     print('Address Mapping')
     print('---------------')
-    for i in range(labelcnt[0]):
-        print('%+-8s' % label_list[i], ": 0x{:08x}".format(label_addr_list[i]))
+    for i in range(label_counter[0]):
+        print('%+-8s' % label_list[i], ": 0x{:08x}".format(label_address_list[i]))
